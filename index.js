cc201459564a8a5e6a8fdcd7dd8a3ae8b8785c23
tobias <fernandezmtobias42@gmail.com>
2023-08-07 22:55:16 -0300

    solucin6

--- index.js
+++ index.js
@@ -10,34 +10,34 @@ ConectMongo();
 
 //directorio publico
 app.use(cors())
-//var whitelist = ['https://serene-biscochitos-a1ead1.netlify.app',
-//"https://mern-calendar-app-production-1e03.up.railway.app/auth/login",
-//"https://mern-calendar-app-production-1e03.up.railway.app/events/create",
-//"https://mern-calendar-app-production-1e03.up.railway.app/events/read",
-//"https://mern-calendar-app-production-1e03.up.railway.app/events/delete",
-//"https://mern-calendar-app-production-1e03.up.railway.app/events/create",
-//"https://mern-calendar-app-production-1e03.up.railway.app/events/update",
-//'https://mern-calendar-app-production-1e03.up.railway.app',
-//"https://mern-calendar-app-production-1e03.up.railway.app/auth/new"
-//
-//
-//]
-//var corsOptions = {
-//  origin: function (origin, callback) {
-//    if (whitelist.indexOf(origin) !== -1) {
-//      callback(null, true)
-//    } else {
-//      callback(new Error('Not allowed by CORS'))
-//    }
-//  }
-//}
+var whitelist = ['https://serene-biscochitos-a1ead1.netlify.app',
+"https://mern-calendar-app-production-1e03.up.railway.app/auth/login",
+"https://mern-calendar-app-production-1e03.up.railway.app/events/create",
+"https://mern-calendar-app-production-1e03.up.railway.app/events/read",
+"https://mern-calendar-app-production-1e03.up.railway.app/events/delete",
+"https://mern-calendar-app-production-1e03.up.railway.app/events/create",
+"https://mern-calendar-app-production-1e03.up.railway.app/events/update",
+'https://mern-calendar-app-production-1e03.up.railway.app',
+"https://mern-calendar-app-production-1e03.up.railway.app/auth/new"
+
+
+]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 
 //rutas
 //lectura y parseo del body(info que recibo desde postaman en un POST)
 app.use(express.json());
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/events',require('./routes/eventsRoutes'));
+app.use('/api/auth',cors(corsOptions),require('./routes/auth'));
+app.use('/api/events',cors(corsOptions),require('./routes/eventsRoutes'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
