80bef4aec4aed9d4fe514ea8475697dbb8f05ac5
tobias <fernandezmtobias42@gmail.com>
2023-08-07 22:25:18 -0300

    intento2

--- index.js
+++ index.js
@@ -10,16 +10,24 @@ ConectMongo();
 
 //directorio publico
 app.use(cors())
-//var whitelist = ['https://serene-biscochitos-a1ead1.netlify.app']
-//var corsOptions = {
-//  origin: function (origin, callback) {
-//    if (whitelist.indexOf(origin) !== -1) {
-//      callback(null, true)
-//    } else {
-//      callback(new Error('Not allowed by CORS'))
-//    }
-//  }
-//}
+var whitelist = ['https://serene-biscochitos-a1ead1.netlify.app',
+"https://mern-calendar-app-production-dd16.up.railway.app/auth/login",
+"https://mern-calendar-app-production-dd16.up.railway.app/events/create",
+"https://mern-calendar-app-production-dd16.up.railway.app/events/read",
+"https://mern-calendar-app-production-dd16.up.railway.app/events/delete",
+"https://mern-calendar-app-production-dd16.up.railway.app/events/create",
+"https://mern-calendar-app-production-dd16.up.railway.app/events/update"
+
+]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 //rutas
 //lectura y parseo del body(info que recibo desde postaman en un POST)
 app.use(express.json());
